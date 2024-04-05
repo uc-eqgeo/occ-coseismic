21570eeb2102db55aad99e7b7428ff610a571c40
JackDMcGrath <44168380+JackDMcGrath@users.noreply.github.com>
2024-04-05 17:39:57 +1300

    Assign NSHM squares based on vertical height to fault mesh elements

--- subduction/discretize_subduction.py
+++ subduction/discretize_subduction.py
@@ -98,7 +98,7 @@ for i, trace in traces.iterrows():
                         'up_depth_km': [up_depth], 'low_depth_km': [low_depth]}, index=[int(trace.FaultID)])
     df1 = pd.concat([df1, df2])
 
-    df2_rectangle_centroid = pd.DataFrame({'fault_id': [trace.FaultID]}, index=[int(trace.FaultID)])
+    df2_rectangle_centroid = pd.DataFrame({'fault_id': [trace.FaultID], 'depth': [np.mean([trace.UpDepth, trace.LowDepth])]}, index=[int(trace.FaultID)])
     df_rectangle_centroid = pd.concat([df_rectangle_centroid, df2_rectangle_centroid])
     #######################
 
@@ -185,16 +185,47 @@ rectangle_rake = np.array(rectangle_rake)
 
 # find the closest rectangle to each triangle centroid
 closest_rectangles = []
-for triangle_centroid in triangle_centroids:
+for ix, triangle_centroid in enumerate(triangle_centroids):
     distances = np.linalg.norm(all_rectangle_centroids - triangle_centroid, axis=1)
     if distances.min() < 2.2e4:
-        closest_rectangle = np.argmin(distances)
+        nearest = np.where(distances < 2.2e4)[0]
+        vsep = all_rectangle_centroids[:, 2] - triangle_centroid[2]
+        abs_sep = abs(vsep)
+        if np.sum(distances < 2.2e4) == 1:  # If only one option, use that option
+            closest_rectangle = np.argmin(distances)
+        else:
+            nearest2 = nearest[np.argsort(abs_sep[nearest])[:2]]
+            if np.diff(abs_sep[nearest2])[0] < 1e3:  # If nearest 2 are within 1 km vertical seperation, use the geographically nearest
+                closest_rectangle = nearest2[np.argmin(distances[nearest2])]
+            else:
+                closest_rectangle = nearest2[np.argmin(abs_sep[nearest2])]  # If nearest 2 are > 1 km vertical seperation, use the structurally nearest
         closest_rectangles.append(closest_rectangle)
     else:
         closest_rectangles.append(-1)
 
-closest_rectangles = np.array(closest_rectangles)
+# Manually correct some triangles
+if os.path.exists('../data/mesh_corrections.csv'):
+    with open('../data/mesh_corrections.csv', 'r') as f:
+        corrections = [[int(val) for val in line.strip().split(',')] for line in f.readlines()]
 
+    for tri, closest_rectangle in corrections:
+        closest_rectangles[tri] = closest_rectangle
+
+# Prevent isolated triangles
+if os.path.exists('../data/hik_kerk3k_neighbours.txt'):
+    with open('../data/hik_kerk3k_neighbours.txt', 'r') as f:
+        neighbours = [[int(tri) for tri in line.strip().split()] for line in f.readlines()]
+
+    for tri in range(len(closest_rectangles)):
+        rect = closest_rectangles[tri]
+        if rect != -1:
+            if len(neighbours[tri]) == 3:
+                neigh = [closest_rectangles[neigh] for neigh in neighbours[tri] if closest_rectangles[neigh] != -1]
+                if all([ix == rect for ix in neigh]) and len(neigh) == 3:
+                    rects, count = np.unique(neigh, return_counts=True)
+                    closest_rectangles[tri] = rects[np.argmax(count)]
+
+closest_rectangles = np.array(closest_rectangles)
 
 # Create polygons from triangles
 discretized_polygons = []
@@ -207,7 +238,7 @@ for index in traces.index:
     triangle_polygons = [Polygon(triangle) for triangle in triangles]
     dissolved_triangle_polygons = gpd.GeoSeries(triangle_polygons).unary_union
     discretized_polygons.append(dissolved_triangle_polygons)
-    discretised_dict[index]= {"triangles": triangles, "rake": rectangle_rake[index]}
+    discretised_dict[index] = {"triangles": triangles, "rake": rectangle_rake[index]}
 
 # Create a geodataframe and geospon file from the polygons
 gdf = gpd.GeoDataFrame({"rake": rectangle_rake, "geometry": discretized_polygons, "fault_id": traces.index})
@@ -215,10 +246,3 @@ gdf.crs = traces.crs
 gdf.to_file(f"out_files{version_extension}/sz_discretized_polygons.geojson", driver="GeoJSON")
 
 pkl.dump(discretised_dict, open(f"out_files{version_extension}/sz_discretised_dict.pkl", "wb"))
-
-
-
-
-
-
-
