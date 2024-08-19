b76063b4ebae36e6aba617c588337a10ccb882d4
JackDMcGrath <44168380+JackDMcGrath@users.noreply.github.com>
2024-08-20 11:47:10 +1200

    Make the same as rtd tutorial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'occ-coseismic'
-copyright = '2024, McGrath, Delano, Howell et al.'
-author = 'Jack McGrath, Jaime Delano, Andy Howell, et al.'
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
 release = '0.1'
 version = '0.1.0'
