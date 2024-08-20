7d8774be531edf2c78aa879ee21447adcb90812b
JackDMcGrath <44168380+JackDMcGrath@users.noreply.github.com>
2024-08-20 12:00:07 +1200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'occ-coseismic'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2024, McGrath, Delano, Howell et al.'
+author = 'Jack McGrath, Jaime Delano, Andy Howell et al.'
 
 release = '0.1'
 version = '0.1.0'
